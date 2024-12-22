3d9f70bce9dfbbf660d984a484bc70d424259103
mharris162 <mharris162@student.alamo.edu>
2024-12-22 16:34:54 -0600

    web formatting grid

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-window.onSpotifyIframeApiReady = (IFrameAPI) => {
-    const element = document.getElementById('embed-iframe');
-    const options = {
-        uri: 'spotify:playlist:1tHgRPvrjdADidyjLDr6kX'
-      };
-    const callback = (EmbedController) => {};
-    IFrameAPI.createController(element, options, callback);
-  };
-  
-  document.getElementById('encore').onclick = function () {
+document.getElementById('encore').onclick = function () {
     window.open('https://www.encore-wd.com/', '_blank');
   };
 
-  document.getElementById('coding').onclick = function () {
+document.getElementById('coding').onclick = function () {
     window.open('https://mharris162.github.io/mharris_JS-Final/', '_blank');
   };
 
-  document.getElementById('todo').onclick = function () {
+document.getElementById('todo').onclick = function () {
     window.open('https://mharris162.github.io/to-do-list/', '_blank');
   };
 
-  document.getElementById('rock').onclick = function () {
+document.getElementById('rock').onclick = function () {
     window.open('https://mharris162.github.io/mharris-rock-paper-scissors/', '_blank');
   };
 
